81e6dacac8228d707ef5dd0b14d47c66d0e680bf
dramak1ng <kensi11qw@gmail.com>
2025-02-18 18:18:25 +0300

    complete task 2.1.2 2.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,19 +2,23 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.logging.Logger;
+
 public class App {
+    private final static Logger logger  = Logger.getLogger(App.class.getName());
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        logger.info(bean.getMessage());
         Cat catBean = applicationContext.getBean("cat", Cat.class);
         Cat catBean2 = applicationContext.getBean("cat", Cat.class);
         boolean compare = bean == bean2;
         boolean compare2 = catBean == catBean2;
-        System.out.println("сравнение hellowordBean: " + compare);
-        System.out.println("сравнение catBean: " + compare2);
+        logger.info("сравнение hellowordBean: " + compare);
+        logger.info("сравнение catBean: " + compare2);
     }
 }
