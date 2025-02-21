db7aaec0ce2e6e7369ab771f635c47f51a87c792
Serafim <kensi11qw@gmail.com>
2025-02-21 13:44:18 +0300

    Delete src/main directory

--- src/main/java/web/exception/UserNotFoundExp.java
+++
@@ -1,9 +0,0 @@
-package web.exception;
-
-public class UserNotFoundExp extends RuntimeException {
-
-    public UserNotFoundExp(int id){
-        super("Пользователь "+id+ " не найден");
-    }
-
-}
